b3151d6a3d2b605c8f08ab06d9967b7efde28795
mistavin <vinblaze090@gmail.com>
2022-09-10 06:29:37 -0700

    added comment to code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-
+//if the number is greater than 0: is positive
+//if the number is 0: is zero
+//if the number is less than 0: is negative
+//return:0
 int main(void)
 {
 int n;
